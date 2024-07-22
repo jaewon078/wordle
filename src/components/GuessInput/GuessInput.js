2fcc14309beaa1cda43704ff6b4c315615ac3977
jaewon078 <lee.jaewon078@gmail.com>
2024-07-22 15:33:25 -0400

    complete objective 1 (add form)

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({ guess });
+    setGuess("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        minLength={5}
+        maxLength={5}
+        value={guess}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        onChange={(event) => {
+          const nextGuess = event.target.value.toUpperCase();
+          setGuess(nextGuess);
+        }}
+        id="guess-input"
+        type="text"
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
